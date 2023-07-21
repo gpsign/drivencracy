d1156b56ef423ff78c6be760d9d2b8bb36a0a99e
gpsign <gpsignorini@gmail.com>
2023-07-21 12:42:21 -0300

    Fix app deploy

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import router from "./routes/index.routes.js";
 
-const app = express()
-app.use(express.json())
-dotenv.config()
+const app = express();
+app.use(express.json());
+dotenv.config();
+app.use(router);
+
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
