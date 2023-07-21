bece571180e3a3d9a1e518da3991e0e5c0ef6c2a
gpsign <gpsignorini@gmail.com>
2023-07-21 00:35:22 -0300

    Mudado organizacao das collections e logica do resultado

--- src/controllers/drivencracy.controller.js
+++ src/controllers/drivencracy.controller.js
@@ -18,8 +18,7 @@ export async function postPoll(req, res) {
 				: dayjs().add(30, "day").format("YYYY-MM-DD HH:MM"),
 		};
 
-		console.log(req.body.expireAt);
-		await db.collection("poll").insertOne(poll);
+		await db.collection("polls").insertOne(poll);
 		return res.status(201).send(poll);
 	} catch (err) {
 		return res.status(500).send(err);
@@ -28,7 +27,7 @@ export async function postPoll(req, res) {
 
 export async function getPoll(req, res) {
 	try {
-		return res.send(await db.collection("poll").find().toArray());
+		return res.send(await db.collection("polls").find().toArray());
 	} catch (err) {
 		return res.status(500).send(err);
 	}
@@ -43,26 +42,24 @@ export async function postChoice(req, res) {
 
 	try {
 		const poll = await db
-			.collection("poll")
+			.collection("polls")
 			.findOne({ _id: new ObjectId(req.body.pollId) });
 
 		if (!poll) {
 			return res.sendStatus(404);
 		}
 
-		console.log(dayjs().diff(poll.expireAt, "day"));
-
 		if (dayjs().diff(poll.expireAt, "day") > 0) {
 			return res.sendStatus(403);
 		}
 
 		const repeated = await db
-			.collection(`${poll.title}`)
+			.collection("choices")
 			.findOne({ title: req.body.title });
 
 		if (repeated) return res.sendStatus(409);
 
-		await db.collection(`${poll.title}`).insertOne(req.body);
+		await db.collection("choices").insertOne(req.body);
 		return res.status(201).send(req.body);
 	} catch (err) {
 		console.log(err);
@@ -73,13 +70,95 @@ export async function postChoice(req, res) {
 export async function getChoices(req, res) {
 	try {
 		const { id } = req.params;
-		const poll = await db.collection("poll").findOne({ _id: new ObjectId(id) });
+		const poll = await db
+			.collection("polls")
+			.findOne({ _id: new ObjectId(id) });
 
 		if (!poll) return res.sendStatus(404);
 
-		const choices = await db.collection(`${poll.title}`).find().toArray();
+		const choices = await db
+			.collection("choices")
+			.find({ pollId: id })
+			.toArray();
 		return res.send(choices);
 	} catch (err) {
+		return res.status(500).send(err);
+	}
+}
+
+export async function postVote(req, res) {
+	try {
+		const { id } = req.params;
+		const choice = await db
+			.collection("choices")
+			.findOne({ _id: new ObjectId(id) });
+
+		if (!choice) {
+			return res.sendStatus(404);
+		}
+
+		const poll = await db
+			.collection("poll")
+			.find({ _id: new ObjectId(choice.pollId) });
+		if (dayjs(choice.expireAt).diff(poll.expireAt, "day") > 0) {
+			return res.sendStatus(403);
+		}
+
+		await db.collection("votes").insertOne({
+			createdAt: dayjs().format("YYYY-MM-DD HH:MM"),
+			choiceId: new ObjectId(id),
+		});
+
+		return res.sendStatus(201);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).send(err);
+	}
+}
+
+export async function getResult(req, res) {
+	try {
+		const { id } = req.params;
+
+		const poll = await db
+			.collection("polls")
+			.findOne({ _id: new ObjectId(id) });
+
+		if (!poll) {
+			return res.sendStatus(404);
+		}
+
+		const choices = await db
+			.collection("choices")
+			.find({ pollId: id })
+			.toArray();
+
+		let finalPoll = {
+			title: poll.title,
+			expireAt: poll.expireAt,
+			result: {
+				title: "",
+				votes: 0,
+			},
+		};
+
+		for (let i = 0; i < choices.length; i++) {
+			console.log(choices[i]._id);
+			let votes = await db
+				.collection("votes")
+				.find({ choiceId: choices[i]._id }).toArray();
+
+			console.log(votes);
+
+			if (votes.length > finalPoll.result.votes) {
+				finalPoll.result.votes = votes.length;
+				finalPoll.result.title = choices[i].title;
+			}
+		}
+
+		return res.send(finalPoll);
+	} catch (err) {
+		console.log(err);
 		return res.status(500).send(err);
 	}
 }
--- src/routes/drivencracy.routes.js
+++ src/routes/drivencracy.routes.js
@@ -2,8 +2,10 @@ import { Router } from "express";
 import {
 	getChoices,
 	getPoll,
+	getResult,
 	postChoice,
 	postPoll,
+	postVote,
 } from "../controllers/drivencracy.controller.js";
 
 const drivencracyRouter = Router();
@@ -12,5 +14,7 @@ drivencracyRouter.post("/poll", postPoll);
 drivencracyRouter.get("/poll", getPoll);
 drivencracyRouter.post("/choice", postChoice);
 drivencracyRouter.get("/poll/:id/choice", getChoices);
+drivencracyRouter.post("/choice/:id/vote", postVote);
+drivencracyRouter.get("/poll/:id/result", getResult);
 
 export default drivencracyRouter;
