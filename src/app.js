9f75c96714ca25aac327ebe47570d426dd269ee2
gpsign <gpsignorini@gmail.com>
2023-07-21 00:52:54 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
-import express, { json } from "express";
-import cors from "cors";
-import router from "./routes/index.routes.js";
+import express from "express"
+import dotenv from "dotenv"
 
-const app = express();
+const app = express()
+app.use(express.json())
+dotenv.config()
 
-app.use(json());
-app.use(cors());
-app.use(router);
-
-const port = 5000;
-
-app.listen(port, () => console.log(`O servidor está online na porta ${port}`));
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
